8ef7511c5eb5e286e099f225b715bb7cd99f4245
Yuri Camara <yuricamara@gmail.com>
2014-10-16 18:45:59 -0300

    Adjustments

--- dev/all-together/scripts.js
+++ dev/all-together/scripts.js
@@ -2,8 +2,8 @@
 'use strict'
 
 var myApp = angular.module('myApp', [
-	'ui.router',
-	'my-templates'
+	'ui.router'//,
+	// 'my-templates'
 ]);
 
 myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
--- dev/global/app.js
+++ dev/global/app.js
@@ -1,8 +1,8 @@
 'use strict'
 
 var myApp = angular.module('myApp', [
-	'ui.router',
-	'my-templates'
+	'ui.router'//,
+	// 'my-templates'
 ]);
 
 myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
